208df3d69dafc13767e15a38ae598df2d60dd991
Devangi02 <86353531+Devangi02@users.noreply.github.com>
2025-05-24 11:39:13 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 # Pull the Docker image from Docker Hub
-docker rmi devhadiya/simple-python-flask-app:latest
+# docker rmi devhadiya/simple-python-flask-app:latest
 docker pull devhadiya/simple-python-flask-app:latest
 
 # Run the Docker image as a container
